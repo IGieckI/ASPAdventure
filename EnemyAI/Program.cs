b4a8fe819aa5b0afff95577d747c737bdec93222
IGieckI <giacomo.antonelli3@gmail.com>
2021-05-24 22:53:06 +0200

    5/24/2021

--- EnemyAI/Program.cs
+++ EnemyAI/Program.cs
@@ -7,21 +7,19 @@ namespace EnemyAI
 {
     class Program
     {
-        private static MLContext mlContext = new MLContext(seed: 1);
+        // Creo l'MLContext per fare tutte le cose AI
+        private static MLContext mlContext = new MLContext();
+        
+        //Creo quello che sarà il corpo dell'ia
         private static Lazy<PredictionEngine<Input, Output>> PredictionEngine = new Lazy<PredictionEngine<Input, Output>>(CreatePredictionEngine);
 
         public static PredictionEngine<Input, Output> CreatePredictionEngine()
         {
-            // Creo l'MLContext per fare tutte le cose AI
-            MLContext mlContext = new MLContext();
-
             // Carico il modello precedentemente creato
             ITransformer mlModel = mlContext.Model.Load((Directory.GetCurrentDirectory() + "\\Data\\Modello.zip"), out var modelInputSchema);
 
             //Creo un prediction engine in cui poter inserire dei dati per testare se è stata tutta una perdita di tempo o qualche neurone a posto c'è l'ha
-            var predEngine = mlContext.Model.CreatePredictionEngine<Input, Output>(mlModel);
-
-            return predEngine;
+            return mlContext.Model.CreatePredictionEngine<Input, Output>(mlModel);
         }
 
         public static void CreateModel()
@@ -34,8 +32,8 @@ namespace EnemyAI
                                             allowQuoting: true,
                                             allowSparse: false);
 
-            // Creo una pipeline che mi permette di "convertire" i dati dal formato che gli passo io a qualcosa che l'IA possa comprendere
-            // Creo una pipeline che concatena diverse colonne di input(definite nella classe input) in una nuova colonna di output
+            //Creo una pipeline che mi permette di "convertire" i dati dal formato che gli passo io a qualcosa che l'IA possa comprendere quindi
+            //una pipeline che concatena diverse colonne di input(definite nella classe input) in una nuova colonna di output
             var dataProcessPipeline = mlContext.Transforms.Concatenate("Features", new[] { "col0", "col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9", "col10", "col11", "col12" });
 
             // Definisco l'algoritmo che andrò ad utilizzare
@@ -58,7 +56,7 @@ namespace EnemyAI
             CreateModel();
 
 
-            Input sampleData = new Input()
+            Input dati = new Input()
             {
                 Col0 = 26F,
                 Col1 = 11F,
@@ -76,22 +74,22 @@ namespace EnemyAI
             };
 
             //Testo il modello 
-            var risultato = PredictionEngine.Value.Predict(sampleData);
+            var risultato = PredictionEngine.Value.Predict(dati);
 
             Console.WriteLine("Using model to make single prediction -- Comparing actual Col13 with predicted Col13 from sample data...\n\n");
-            Console.WriteLine($"Col0: {sampleData.Col0}");
-            Console.WriteLine($"Col1: {sampleData.Col1}");
-            Console.WriteLine($"Col2: {sampleData.Col2}");
-            Console.WriteLine($"Col3: {sampleData.Col3}");
-            Console.WriteLine($"Col4: {sampleData.Col4}");
-            Console.WriteLine($"Col5: {sampleData.Col5}");
-            Console.WriteLine($"Col6: {sampleData.Col6}");
-            Console.WriteLine($"Col7: {sampleData.Col7}");
-            Console.WriteLine($"Col8: {sampleData.Col8}");
-            Console.WriteLine($"Col9: {sampleData.Col9}");
-            Console.WriteLine($"Col10: {sampleData.Col10}");
-            Console.WriteLine($"Col11: {sampleData.Col11}");
-            Console.WriteLine($"Col12: {sampleData.Col12}");
+            Console.WriteLine($"Col0: {dati.Col0}");
+            Console.WriteLine($"Col1: {dati.Col1}");
+            Console.WriteLine($"Col2: {dati.Col2}");
+            Console.WriteLine($"Col3: {dati.Col3}");
+            Console.WriteLine($"Col4: {dati.Col4}");
+            Console.WriteLine($"Col5: {dati.Col5}");
+            Console.WriteLine($"Col6: {dati.Col6}");
+            Console.WriteLine($"Col7: {dati.Col7}");
+            Console.WriteLine($"Col8: {dati.Col8}");
+            Console.WriteLine($"Col9: {dati.Col9}");
+            Console.WriteLine($"Col10: {dati.Col10}");
+            Console.WriteLine($"Col11: {dati.Col11}");
+            Console.WriteLine($"Col12: {dati.Col12}");
             Console.WriteLine($"\n\nPredicted Col13: {risultato.Score}\n\n");
         }
     }
